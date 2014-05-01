f87d85bec5f2e2a1d15118561ba8d3b8c546fa22
jugador87 <jugador87@gmail.com>
2014-05-01 15:46:42 -0700

    Initial Context class creation

---
+++ Context.py
@@ -0,0 +1,13 @@
+
+# This class defines the context object for a particular entity based on XCML
+class ContextObject:
+
+    def __init__(self):
+        self.contextSpace = []
+        
+# This class defines all the variables that constitute the context space
+class ContextSpace:
+
+    def __init__(self):
+        self.contextPropertyNames = []
+
