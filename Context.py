e2e25be8e196d32ff81ed37159fb32a2fe222b60
jugador87 <jugador87@gmail.com>
2014-05-01 17:16:12 -0700

    Context Class fleshed out.
    Next step: Component model from here:
    http://www.globis.ethz.ch/publications/?action=show_abstract&pubid=344

--- Context.py
+++ Context.py
@@ -3,11 +3,24 @@
 class ContextObject:
 
     def __init__(self):
+        # This must consist of properties found in
+        # ContextSpace.contextPropertyNames
         self.contextSpace = []
+
+        # This is the current context for this entity: a mapping of each context
+        # in self.contextSpace to its value. Querying this at any given time
+        # should give you the current context. Not all elements may be filled.
+        self.context = {}
+
+        # This is the list of context names that are currently deemed relevant
+        # to this entity/application
+        self.dimension = []
         
 # This class defines all the variables that constitute the context space
 class ContextSpace:
 
     def __init__(self):
-        self.contextPropertyNames = []
+        # This variable provides a mapping of all available context NAMES
+        # to their associated range of VALUES (currently discrete)
+        self.contextPropertyNames = {}
 
